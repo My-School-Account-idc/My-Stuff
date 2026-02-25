57d5ab078d15dd67528a061f7402fa90c6995eb6
My-School-Account-idc <1797119@fcpsschools.net>
2026-02-25 11:20:54 -0500

    Add files via upload

---
+++ Guess The Number.py
@@ -0,0 +1,23 @@
+31
+Guess a number between 1 and 100!
+
+Type Your Guess! (Use Only Numbers)20
+
+Pick a Bigger Number! Your guess was too low!
+
+
+Type Your Guess! (Use Only Numbers)21
+
+Pick a Bigger Number! Your guess was too low!
+
+
+Type Your Guess! (Use Only Numbers)31
+
+The Answer was Smaller than 67!
+
+You Are Correct! The answer was 31
+
+Your Score Was: 70
+
+
+** Process exited - Return Code: 0 **
